9babe17cdc613d6425784c96797d5e882c8ecb6d
izzy <me@insrt.uk>
2026-03-16 12:57:13 -0500

    chore: update mise & tools

--- default.nix
+++ default.nix
@@ -1,13 +1,14 @@
-{
-  pkgs ? import <nixpkgs> { },
+{ pkgs ? import (fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/77ef7a29d276c6d8303aece3444d61118ef71ac2.tar.gz";
+    sha256 = "0pm4l48jq8plzrrrisimahxqlcpx7qqq9c99hylmf7p3zlc3phsy";
+  }) {},
 }:
 
-with pkgs;
 pkgs.mkShell {
-  buildInputs = [
-    restic
-    zizmor
-    nodejs
-    nodejs.pkgs.pnpm
+  packages = with pkgs; [
+    mise
+    (writeShellScriptBin "fish" ''
+      exec ${pkgs.fish}/bin/fish -C 'mise activate fish | source' "$@"
+    '')
   ];
 }
