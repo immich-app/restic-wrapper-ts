02409d2c0d818201297cf76b3896f16ce1c0b43d
izzy <me@insrt.uk>
2026-03-16 13:53:02 -0500

    chore: bump mise again
    Signed-off-by: izzy <me@insrt.uk>

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 { pkgs ? import (fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/77ef7a29d276c6d8303aece3444d61118ef71ac2.tar.gz";
-    sha256 = "0pm4l48jq8plzrrrisimahxqlcpx7qqq9c99hylmf7p3zlc3phsy";
+    url = "https://github.com/NixOS/nixpkgs/archive/a07d4ce6bee67d7c838a8a5796e75dff9caa21ef.tar.gz";
+    sha256 = "0f6zni3jn6ji5icwbidbpmcgxdal2qnjszp7ragdcy0857hvq3c5";
   }) {},
 }:
 
